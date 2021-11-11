8771e58eb5802f1097af26081f059f4207980b8a
Alexander96779 <niyialexp@gmail.com>
2021-11-11 11:24:13 +0200

    Feature heroku DB configurations done!

---
+++ src/config/config.js
@@ -0,0 +1,21 @@
+import { config } from 'dotenv';
+
+config();
+
+export default {
+
+  "development": {
+    "username": process.env.DATABASE_USERNAME,
+    "password": null,
+    "database": process.env.DATABASE_URL_DEV,
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "use_env_variable": 'DATABASE_URL_PROD',
+    "dialect": 'postgres',
+    "dialectOptions": {
+      ssl: { require: true, rejectUnauthorized: false },
+    }
+  }
+};
